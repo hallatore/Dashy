6d4e08d00d1842c45458a146ee5e02750b189bed
Tore Lervik <tore.lervik@bekk.no>
2020-11-27 23:50:41 +0100

    Added exception catching

--- Dashy/App.xaml.cs
+++ Dashy/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace Dashy
@@ -11,6 +13,26 @@ namespace Dashy
         {
             base.OnStartup(e);
             Profile = e.Args.FirstOrDefault() ?? "settings";
+
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                LogUnhandledException((Exception)e.ExceptionObject);
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception);
+                e.Handled = true;
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception);
+                e.SetObserved();
+            };
+        }
+
+        private void LogUnhandledException(Exception exception)
+        {
+            MessageBox.Show(exception.ToString(), exception.Message, MessageBoxButton.OK);
         }
     }
 }
